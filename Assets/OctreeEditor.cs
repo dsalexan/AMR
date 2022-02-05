b96784c43b41b47d79f188038fa3f52c44d05b36
Danilo Alexandre <danilo.salexan@gmail.com>
2022-02-05 13:21:37 -0300

    prototyping

--- Assets/OctreeEditor.cs
+++ Assets/OctreeEditor.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+[CustomEditor(typeof(OctreeObject))]
 public class OctreeEditor : Editor 
 {
     public override void OnInspectorGUI()
     {
-        if (GUILayout.Button("Subdivide"))
-        {
-            
-        }
+        base.OnInspectorGUI();
+
+        OctreeObject obj = (OctreeObject)target;
+        
+        if (GUILayout.Button("Create")) obj.CreateMesh();
+        if (GUILayout.Button("Render")) obj.Render();
     }
 }
--- Assets/OctreeObject.cs
+++ Assets/OctreeObject.cs
@@ -8,9 +8,73 @@ public class OctreeObject : MonoBehaviour
 {
     public Octree.Tree<int> tree;
 
+    public Mesh mesh;
+
+    public Vector3 Dimensions = new Vector3(1f, 1f, 1f);
+    public Vector3 Cells = new Vector3(10, 10, 10);
+
+    public void Render()
+    {
+        MeshFilter filter = GetComponent<MeshFilter>();
+
+        if (mesh == null) mesh = new Mesh();
+        filter.mesh = mesh;
+    }
+
+    public void CreateMesh()
+    {
+        if (mesh == null) mesh = new Mesh();
+
+        int I = (int)Cells[0];
+        int J = (int)Cells[1];
+        int K = (int)Cells[2];
+
+        float X = Dimensions[0];
+        float Y = Dimensions[1];
+        float Z = Dimensions[2];
+
+        float dX = X / (I - 1);
+        float dY = Y / (J - 1);
+        float dZ = Z / (K - 1);
+
+        List<Vector3> vertices = new List<Vector3>();
+
+        float x0 = -X / 2f;
+        float y0 = -Y / 2f;
+        float z0 = -Z / 2f;
+        
+        for (int i = 0; i < I; i++)
+        {
+            for (int j = 0; j < J; j++)
+            {
+                for (int k = 0; k < K; k++)
+                {
+                    Vector3 vertex = new Vector3
+                    {
+                        x = i * dX + x0,
+                        y = j * dY + y0,
+                        z = k * dZ + z0
+                    };
+                    vertices.Add(vertex);
+                }
+            }
+        }
+
+        mesh.SetVertices(vertices);
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(Vector3.zero, new Vector3(10f, 10f, 10f));
+        Gizmos.DrawWireCube(this.transform.position, this.transform.TransformVector(Dimensions));
+
+        if (mesh != null)
+        {
+            Gizmos.color = Color.red;
+            foreach (Vector3 vertex in mesh.vertices)
+            {
+                Gizmos.DrawSphere(transform.TransformPoint(vertex), 0.1f);
+            }
+        }
     }
 }
